15e4ba8b173ca38e853b38cdf5d245ccad5d0d5a
PIETRO MARCHIORATO <120272502+pietromarchiorato@users.noreply.github.com>
2024-04-07 18:11:38 +0200

    Add files via upload

--- backend/Model/Core/Result.py
+++ backend/Model/Core/Result.py
@@ -1,11 +1,11 @@
 class Result:
     def __init__(self, data):
         self.data = data
-        self.processed = false
+        self.processed = False
 
     def get(self):
         if not self.processed:
-            self.processed = true
+            self.processed = True
             #similarities = self.search_algorithm.search(list(self.tc_embs.values()), query_embedding)
             threshold = 0.75
             #matches = [name for name, sim in zip(self.tc_embs.keys(), similarities) if sim.item() > threshold]
