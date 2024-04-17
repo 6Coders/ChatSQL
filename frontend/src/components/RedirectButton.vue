4665f832d79400cea1f65ae6ce668625dbbca890
unknown <y.lovato01@gmail.com>
2024-04-17 15:09:03 +0200

    Aggiunto redirectbutton

---
+++ frontend/src/components/RedirectButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <GenericButton :buttonClass="buttonClass" @clicked="handleClick">
+      <slot></slot>
+    </GenericButton>
+  </template>
+  
+  <script>
+  import GenericButton from "./GenericButton.vue";
+  
+  export default {
+    components: {
+      GenericButton
+    },
+    props: {
+      buttonClass: {
+        type: String,
+        default: "generic-button"
+      },
+      destination: {
+        type: String,
+        required: true 
+      }
+    },
+    methods: {
+      handleClick() {
+        window.location.href = this.destination;
+      }
+    }
+  };
+</script>
+  
