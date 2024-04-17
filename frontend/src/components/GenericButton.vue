8c7870e73d310b2e49140714fb71e00acb93f035
unknown <y.lovato01@gmail.com>
2024-04-17 15:08:39 +0200

    Aggiunto il componente bottone generico

---
+++ frontend/src/components/GenericButton.vue
@@ -0,0 +1,21 @@
+<template>
+      <button :class="buttonClass" @click="handleClick">
+    <slot></slot>
+  </button>
+</template>
+  
+<script>
+  export default {
+    props: {
+      buttonClass: {
+        type: String,
+        default: "generic-button" 
+      }
+    },
+    methods: {
+      handleClick() {
+        this.$emit("clicked");
+      }
+    }
+  };
+</script>
