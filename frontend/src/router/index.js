ed4d61eee2a03f25f374fee2df1c140754862aa7
unknown <y.lovato01@gmail.com>
2024-04-17 15:10:50 +0200

    Modificato app.vue e index.js per il routing dell'applicazione

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,24 +1,29 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+import HomePage from '../views/HomePage.vue'
+import ManagerPage from '../views/ManagerPage.vue'
+import RequestPage from '../views/RequestPage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: HomePage
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/manager',
+    name: 'manager',
+    component: ManagerPage
+  },
+  {
+    path: '/request',
+    name: 'request',
+    component: RequestPage
+  },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
