ff78bea82a1328149540540b3429a95d4ab66d47
unknown <y.lovato01@gmail.com>
2024-04-17 15:09:45 +0200

    Aggiunte le viste dell'applicazione

---
+++ frontend/src/views/HomePage.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <p>Manager page<RedirectButton :buttonClass="buttonClass" :destination="destinationManager">Vai alla pagina Manager</RedirectButton></p>
+      <p>Request page<RedirectButton :buttonClass="buttonClass" :destination="destinationRequest">Vai alla pagina Request</RedirectButton></p>
+    </div>
+</template>
+  
+  <script>
+  import RedirectButton from "../components/RedirectButton.vue";
+  
+  export default {
+    components: {
+      RedirectButton
+    },
+    data() {
+      return {
+        buttonClass: "specific-button",
+        destinationManager: "/manager",
+        destinationRequest: "/request"
+      };
+    }
+  };
+  </script>
+  
---
+++ frontend/src/views/ManagerPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Manager Page</h1>
+      <p>Contenuto della pagina di richiesta...</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Logica del componente, se necessario
+  }
+  </script>
