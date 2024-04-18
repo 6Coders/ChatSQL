9dac0ef4f23b269b60eb7992efd3500ec690b148
Osama Chelhaoui <oslaman@proton.me>
2024-04-18 18:59:30 +0200

    prima versione di requestpage

---
+++ frontend/src/components/SendRequestButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="sendMessage">
+      <slot></slot>
+    </button>
+  </template>
+  
+  <script>  
+  export default {
+    props: {
+      message: {
+        type: String,
+        required: true
+      }
+    },
+    setup() {
+      async function sendMessage() {
+        
+      }
+
+      return {
+        sendMessage
+      }
+    }
+  }
+  </script>
--- frontend/src/views/RequestPage.vue
+++ frontend/src/views/RequestPage.vue
@@ -1,17 +1,69 @@
 <template>
   <div>
     <h1>Request Page</h1>
-    <p>Contenuto della pagina di richiesta...</p>
-    <input-file></input-file>
+
+    <ul>
+      <li v-for="(message, index) in messages" :key="index">
+        {{ message }}
+      </li>
+    </ul>
+
+    <form @submit.prevent="submitForm">
+      <input v-model="requestMessage" name="requestMessage">
+      <button type="submit" v-if="!isSending">Send Message</button>
+      <button type="button" v-else @click="stopSending">Ferma</button>
+    </form>
   </div>
 </template>
 
 <script>
-import InputFile from '../components/InputFile.vue';
+import { ref } from 'vue'
 
 export default {
-  components: {
-    InputFile
-  },
+  setup() {
+    const requestMessage = ref('')
+    const messages = ref([])
+    const isSending = ref(false)
+    let shouldStop = false
+
+    const simulateAsyncCall = () => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (shouldStop) {
+            reject('Stopped')
+          } else {
+            resolve('Richiesta inviata: ')
+          }
+        }, 2000)
+      })
+    }
+
+    const submitForm = async () => {
+      isSending.value = true
+      const sentMessage = 'Invio richiesta: ' + requestMessage.value
+      messages.value.push(sentMessage, 'Loading...')
+      shouldStop = false
+      try {
+        const responseMessage = await simulateAsyncCall()
+        messages.value[messages.value.length - 1] = responseMessage + sentMessage
+      } catch (error) {
+        messages.value.pop()
+      }
+      isSending.value = false
+      requestMessage.value = ''
+    }
+
+    const stopSending = () => {
+      shouldStop = true
+    }
+
+    return {
+      requestMessage,
+      messages,
+      submitForm,
+      isSending,
+      stopSending
+    }
+  }
 }
 </script>
