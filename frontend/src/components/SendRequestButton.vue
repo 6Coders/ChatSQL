df3b9c9fcb1f3670e8e078737397c01eea0c41b6
Osama Chelhaoui <oslaman@proton.me>
2024-04-23 19:40:53 +0200

    spostata logica di invio della richiesta a vmrequest

--- frontend/src/components/SendRequestButton.vue
+++ frontend/src/components/SendRequestButton.vue
@@ -1,68 +1,19 @@
 <template>
-  <button :class="sendButtonClass" type="button" @click="submitForm" v-if="!isSending">Invia</button>
-  <button :class="stopSendButtonClass" type="button" title="Interrompi" @click="stopSending" v-else>Interrompi</button>
+  <button :class="sendButtonClass" type="button" @click="submitMethod" v-if="!status">Invia</button>
+  <button :class="stopSendButtonClass" type="button" title="Interrompi" @click="stopSubmitMethod" v-else>Interrompi</button>
 </template>
 
 <script>
-import { ref } from 'vue'
-import axios from 'axios'
 export default {
   name: 'SendRequestButton',
   props: {
-    requestMessage: String,
     sendButtonClass: String,
     stopSendButtonClass: String,
-  },
-  setup(props, { emit }) {
-    const isSending = ref(false)
-    const CancelToken = axios.CancelToken
-    let cancel
-
-    async function generatePrompt() {
-      return new Promise((resolve, reject) => {
-        axios.post('/generateprompt', { userRequest: props.requestMessage }, {
-          cancelToken: new CancelToken(function executor(c) {
-            cancel = c
-          })
-        })
-          .then(response => {
-            resolve(response.data.result);
-          })
-          .catch(error => {
-            if (axios.isCancel(error)) {
-              reject('Request canceled')
-            } else {
-              reject(error)
-            }
-          })
-      })
+    submitMethod: Function,
+    stopSubmitMethod: Function,
+    status: {
+      type: Boolean,
+      default: false
     }
-
-    async function submitForm() {
-      isSending.value = true
-      emit('request', props.requestMessage)
-      try {
-        const responseMessage = await generatePrompt()
-        emit('submit', responseMessage)
-      } catch (error) {
-        emit('submit', 'Stopped')
-        console.error(error)
-      } finally {
-        isSending.value = false
-      }
-    }
-
-    const stopSending = () => {
-      if (cancel) {
-        cancel()
-      }
-    }
-
-    return {
-      submitForm,
-      stopSending,
-      isSending
-    }
-  }
-}
+  }}
 </script>
