23d1d2009dc16e820e39efccb17758651e9af46e
Vullnet <vuvogli@gmail.com>
2024-04-29 21:58:42 +0200

    Aggiunte eccezioni custom

---
+++ backend/chatsql/utils/Exceptions.py
@@ -0,0 +1,16 @@
+
+
+class CustomException(BaseException):
+
+    def __init__(self, message):
+        self._message = message
+        
+
+    @property
+    def message(self):
+        return self._message
+
+class InvalidStructure(CustomException):
+
+    def __init__(self, message="Invalid file structure"):
+        super().__init__(message)
