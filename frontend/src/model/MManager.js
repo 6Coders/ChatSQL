fe637fbd66b4d86b9feceadd5c036742c29e677f
unknown <y.lovato01@gmail.com>
2024-04-30 12:40:25 +0200

    Modifiche Manager page

--- frontend/src/model/MManager.js
+++ frontend/src/model/MManager.js
@@ -28,6 +28,7 @@ export default {
       return 'Internal Server Error';
     }
   },
+  /*Ritorna la lista dei dizionari presenti a sistema*/
   async getDictionaries()
   {
     try 
@@ -36,16 +37,18 @@ export default {
       console.log('Backend response status:', response.status);  
       if (response.status === 200 || response.status === 201) 
       {
-        console.log('Backend response data:', response.data);  
+        /*Se lo stato 200 || 201 la richiesta è andata a buon fine e ritorno la lista dei dizionari*/
         return response.data;
       } else 
       {
-        return 'Internal Server Error';
+        /*Altrimenti ritorno un messaggio di errore*/
+        return ;
       }
     } catch (error) 
     {
+      /*Errore interno al server*/
       console.error('Internal Server Error:', error);
-      return 'Internal Server Error';
+      return ;
     }
   },
   convalidateFile(file) {
@@ -63,10 +66,25 @@ export default {
     const maxSize = 500 * 1024; 
     return file.size <= maxSize;
   },
+  
   async deleteDictionary(id) {
-    console.log('Eliminazione del dizionario con id:', id);
-    //implementazione della chiamata al backend
-    return true;
+    try {
+      const response = await axios.delete(`http://localhost:5000/files/${id}`);
+      console.log('Backend response status:', response.status);  
+      if (response.status === 200 || response.status === 204) {
+        // Se lo stato 200 o 204 indica che la richiesta è andata a buon fine
+        console.log('File eliminato con successo');
+        return true; // Oppure potresti restituire un messaggio di successo
+      } else {
+        // Altrimenti ritorna false o un messaggio di errore
+        console.error('Errore durante l\'eliminazione del file');
+        return false; // Oppure potresti gestire diversamente l'errore
+      }
+    } catch (error) {
+      // Errore interno al server o errore di rete
+      console.error('Internal Server Error:', error);
+      return false; // Oppure potresti gestire diversamente l'errore
+    }
   },
   async loadDictionary(id) {
     console.log('Caricamento del dizionario con id:', id);
