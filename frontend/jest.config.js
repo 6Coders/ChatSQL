32b3a97f0594e9c620eb2ceeef966c6292117e8d
Vullnet <vuvogli@gmail.com>
2024-05-01 18:24:45 +0200

    Test ui spostati & aggiornato yaml file

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,16 +1,14 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-   // Directory dove Jest deve cercare i test
-   testMatch: ['<rootDir>/tests/**/*.spec.js'],
-  
-   // Directory dove Jest deve cercare i moduli
-   moduleDirectories: ['node_modules', 'src'],
-   
-   // Indica se Jest deve raccogliere la copertura del codice
-   collectCoverage: true,
-   
-   // Directory dove Jest deve mettere i risultati della copertura
-   coverageDirectory: '<rootDir>/coverage',
 
-   transformIgnorePatterns: ['node_modules/(?!(axios)/)'],
+  // Directory dove Jest deve cercare i moduli
+  moduleDirectories: ['node_modules', 'src'],
+  
+  // Indica se Jest deve raccogliere la copertura del codice
+  collectCoverage: true,
+  
+  // Directory dove Jest deve mettere i risultati della copertura
+  coverageDirectory: '<rootDir>/coverage',
+
+  transformIgnorePatterns: ['node_modules/(?!(axios)/)'],
 }
