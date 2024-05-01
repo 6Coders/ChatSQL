edf909103dea511ca0baa296482b1d35e7b9b90a
Vullnet <vuvogli@gmail.com>
2024-05-01 16:19:29 +0200

    Fix imports

--- tests/backend/test_jsonvalidator.py
+++ tests/backend/test_jsonvalidator.py
@@ -1,7 +1,8 @@
 import unittest
+from unittest import TestCase
 from backend.chatsql.utils.JSONValidator import JSONValidator
 
-class TestJSONValidator(unittest.TestCase):
+class TestJSONValidator(TestCase):
     def test_is_valid_structure_valid(self):
         json_data = {
             "tables_info": {
@@ -15,7 +16,7 @@ class TestJSONValidator(unittest.TestCase):
     def test_is_valid_structure_invalid(self):
         # Test caso in cui mancano chiavi
         json_data_missing_keys = {"tables_info": {}, "primary_key": {}, "foreign_keys": {}}
-        assert JSONValidator.is_valid_structure(json_data_missing_keys) == False
+        TestCase.assertTrue(self, JSONValidator.is_valid_structure(json_data_missing_keys))
 
         # Test caso in cui le tabelle non hanno tutte le informazioni necessarie
         json_data_invalid_tables_info = {
@@ -23,7 +24,7 @@ class TestJSONValidator(unittest.TestCase):
             "primary_key": {"table1": ["column1"]},
             "foreign_keys": []
         }
-        assert JSONValidator.is_valid_structure(json_data_invalid_tables_info) == False
+        TestCase.assertFalse(self, JSONValidator.is_valid_structure(json_data_invalid_tables_info))
 
         # Test caso in cui le chiavi primarie non sono correttamente strutturate
         json_data_invalid_primary_key = {
@@ -31,15 +32,15 @@ class TestJSONValidator(unittest.TestCase):
             "primary_key": {"table1": "column1"},
             "foreign_keys": []
         }
-        assert JSONValidator.is_valid_structure(json_data_invalid_primary_key) == False
-
+        TestCase.assertFalse(self, JSONValidator.is_valid_structure(json_data_invalid_primary_key))
+        
         # Test caso in cui le chiavi esterne non sono correttamente strutturate
         json_data_invalid_foreign_keys = {
             "tables_info": {"table1": {"table_description": "Description", "columns": [], "attribute_types": []}},
             "primary_key": {"table1": ["column1"]},
             "foreign_keys": [{"table": "table1"}]
         }
-        assert JSONValidator.is_valid_structure(json_data_invalid_foreign_keys) == False
+        TestCase.assertFalse(self, JSONValidator.is_valid_structure(json_data_invalid_foreign_keys))
 
 if __name__ == '__main__':
     unittest.main()
