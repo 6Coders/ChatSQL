8d655303b8c66e0a6fe45f96bef222e95e0a4630
unknown <y.lovato01@gmail.com>
2024-05-02 16:11:19 +0200

    Corretto e aggiunto test

--- frontend/src/components/ToastPopup.vue
+++ frontend/src/components/ToastPopup.vue
@@ -1,12 +1,12 @@
-<template ref="Toast">
-<div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
-  <div class="d-flex">
-    <div class="toast-body">
-      {{ Message }}
+<template>
+  <div ref="toast" class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="d-flex">
+      <div class="toast-body">
+        {{ Message }}
+      </div>
+      <button type="button" class="btn-close btn-close-white me-2 m-auto" aria-label="Close" data-bs-dismiss="toast"></button>
     </div>
-    <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
   </div>
-</div>  
 </template>
 
 <script>
