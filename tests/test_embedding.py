8802f96dd925d87a2e5b722b07b335c136f44724
Vullnet <vuvogli@gmail.com>
2024-05-09 11:36:26 +0200

    Scritti test di unità per classe `Embedding`

--- tests/test_embedding.py
+++ tests/test_embedding.py
@@ -4,8 +4,10 @@ from unittest import TestCase
 from backend.chatsql.domain.Embedding import Embedding
 import numpy as np
 
+
 class TestEmbedding(TestCase):
-    def test_embedding_creation(self):
+
+    def test_correct_creation(self):
         # Arrange
         text = "example text"
         data = np.array([1, 2, 3])
@@ -15,10 +17,32 @@ class TestEmbedding(TestCase):
 
         # Assert
         self.assertEquals(embedding.text, text)
-        self.assertIsNone(np.testing.assert_array_equal(
-            data,
-            embedding.data
-        ))  # Returns None if the arrays are equal
+        self.assertIsNone(
+            # Returns None if the arrays are equal
+            np.testing.assert_array_equal(
+                data,
+                embedding.data
+            )
+        )
+
+    def test_text_and_data_none_creation(self):
+
+        with self.assertRaises(TypeError) as context:
+
+            embedding = Embedding()
+
+    def test_none_text_creation(self):
+
+        with self.assertRaises(ValueError) as context:
+
+            embedding = Embedding(text=None, data=np.array([1, 2, 3]))
+
+    def test_none_data_creation(self):
+
+        with self.assertRaises(ValueError) as context:
+
+            embedding = Embedding(text='default', data=None)
+
 
 if __name__ == '__main__':
     unittest.main()
