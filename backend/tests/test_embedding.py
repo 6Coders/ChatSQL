6e495a387543771dfa98f913739af964752ec4da
Vullnet <vuvogli@gmail.com>
2024-05-13 17:07:45 +0200

    Convertiti i test di `TestHuggingfaceEmbeddingAdapter`

--- backend/tests/test_embedding.py
+++ backend/tests/test_embedding.py
@@ -2,6 +2,8 @@ import unittest
 from unittest import TestCase
 
 from chatsql.domain.Embedding import Embedding
+from chatsql.adapter.incoming.EmbeddingGeneratorAdapters import HuggingfaceEmbeddingAdapter
+
 import numpy as np
 
 
@@ -56,6 +58,23 @@ class TestEmbedding(TestCase):
         with self.assertRaises(ValueError) as context:
 
             embedding = Embedding(text='default', table_name='test', data=np.array([]))
+
+
+class TestHuggingfaceEmbeddingAdapter(unittest.TestCase):
+    def setUp(self):
+        self.adapter = HuggingfaceEmbeddingAdapter()
+
+    def test_generate(self):
+        texts = ['test1', 'test2']
+        table_names = ['first_table', 'second_table']
+        embeddings = self.adapter.generate(texts, table_names)
+
+        self.assertEqual(len(embeddings), len(texts))
+        for embedding in embeddings:
+            self.assertEqual(embedding.text, embedding.text)
+            self.assertEqual(embedding.table_name, embedding.table_name)
+            self.assertTrue(embedding.data.size != 0)
+            self.assertIsInstance(embedding.data, np.ndarray)
 
 
 if __name__ == '__main__':
